b82f8bdc2640f1c0eb713f54902a8480fcb80b32
TsaoYuCheng <max817781@gmail.com>
2026-07-31 13:11:46 +0800

    更新 0731 館藏與歌單資料

--- data/playgroundLinks.js
+++ data/playgroundLinks.js
@@ -1,4 +1,4 @@
-// ?????????
+// 恐龍遊樂場
 const playgroundLinks = [
     {
         "title": "重返半周年 MOD 爭權大賽",
@@ -16,6 +16,12 @@ const playgroundLinks = [
         "title": "67 Rhythm God",
         "desc": "A rhythm game about 67",
         "url": "https://max817781.github.io/67goden/",
+        "external": true
+    },
+    {
+        "title": "回憶陳綺貞",
+        "desc": "陳綺貞歌曲 2選1 挑戰",
+        "url": "https://max817781.github.io/CheerChenGame2pick2.0/",
         "external": true
     }
 ];

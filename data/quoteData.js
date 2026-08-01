366d21460c812a7aeb36418e9f35ffb2ab57b55b
Twitch_sundaycool_live(孩子王) <max817781@gmail.com>
2026-08-01 23:53:33 +0800

    Update YanYan quote collection (#6)

--- data/quoteData.js
+++ data/quoteData.js
@@ -6,11 +6,31 @@ window.museumDataParts.quoteData = [
         "icon": "🦖"
     },
     {
-        "sourceId": "quote_not_goblin",
-        "icon": "🌱"
+        "sourceId": "quote_goblin_over_crocodile",
+        "icon": "🐊"
     },
     {
-        "sourceId": "quote_southern_zongzi",
-        "icon": "🔥"
+        "sourceId": "quote_care_about_dumplings",
+        "icon": "🥟"
+    },
+    {
+        "sourceId": "quote_drowning_singing",
+        "icon": "💧"
+    },
+    {
+        "sourceId": "quote_onedayday",
+        "icon": "☀️"
+    },
+    {
+        "sourceId": "quote_orange_scolding",
+        "icon": "🍊"
+    },
+    {
+        "sourceId": "quote_gentle_and_graceful",
+        "icon": "🌸"
+    },
+    {
+        "sourceId": "quote_onion_evolution",
+        "icon": "🧅"
     }
 ];
